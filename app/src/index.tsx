42ebc3466f8b6a99da3a03771305c8717541d4b4
David Vílchez <dvilchez@xuaps.com>
2022-04-01 18:59:02 +0200

    Add mixpanel

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,9 +1,13 @@
 import React, { Suspense } from 'react';
+import mixpanel from 'mixpanel-browser';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-
 import './i18n';
 
+mixpanel.init('41dc75713862b8d3691e99fcabf57c0d', {
+  debug: process.env.DEGUB === 'true',
+});
+mixpanel.track('App Loaded');
 ReactDOM.render(
   <Suspense fallback="...">{React.createElement(App)}</Suspense>,
   document.getElementById('root'),
